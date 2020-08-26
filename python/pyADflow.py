093479989a81b9597d198db4bcd26707c1e4274e
Sicheng He <hschsc@gl-login2.arc-ts.umich.edu>
2020-08-26 18:03:57 -0400

    fix a bug related with TS wing flutter adjoint

--- python/pyADflow.py
+++ python/pyADflow.py
@@ -4043,7 +4043,7 @@ class ADFLOW(AeroSolver):
                             if self.use_airfoil: # airfoil case HACK
                                 xs0bar = self.surfaceTransfer.setXS_b(xsbar)
                             else: # wing case HACK
-                                cfdPts0 = self.getInitialSurfaceCoordinates()
+                                cfdPts0 = self.getInitialSurfaceCoordinates(HSCflag = False)
                                 xs0bar = self.surfaceTransfer.setXS_b(cfdPts0, xsbar)
 
                             xdvbar.update(self.DVGeo.totalSensitivity(
@@ -4065,6 +4065,7 @@ class ADFLOW(AeroSolver):
                 # Include aero derivatives here:
                 xdvbar.update(self._processAeroDerivatives(extrabar, bcdatavaluesbar))
                 returns.append(xdvbar)
+                # print("kakasss2, pts", self.getSurfaceCoordinates(groupName=None, includeZipper=False, TS=1))
 
 
         # Include the aerodynamic variables if requested to do so and
@@ -4073,6 +4074,7 @@ class ADFLOW(AeroSolver):
             xdvaerobar = {}
             xdvaerobar.update(self._processAeroDerivatives(extrabar, bcdatavaluesbar))
             returns.append(xdvaerobar)
+        # print("kakasss3, pts", self.getSurfaceCoordinates(groupName=None, includeZipper=False, TS=1))
 
         # Single return (most frequent) is 'clean', otherwise a tuple.
         return tuple(returns) if len(returns) > 1 else returns[0]
